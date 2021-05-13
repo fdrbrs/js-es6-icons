2da0a5f474a3e3c381bfe5dcf2345a37ba33e7f4
fdrbrs <federio.borsci@gmail.com>
2021-05-13 16:37:10 +0200

    create arrays for icon type

---
+++ assets/js/main.js
@@ -0,0 +1,121 @@
+/* 
+Milestone 1:
+Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+Milestone 2 Coloriamo le icone per tipo
+Milestone 3 Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+Usate questa struttura dati per le icone:
+*/
+
+
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	}
+];
+
+//creo un ciclo for each per creare 3 array dove metto i diversi type di icone
+let animalIcons = [], vegetableIcons = [], userIcons = [];
+icons.forEach(icon => {
+    if(icon.type === 'animal'){
+        animalIcons.push(icon);
+    } else if (icon.type === 'vegetable'){
+        vegetableIcons.push(icon);
+    } else {
+        userIcons.push(icon)
+    }
+});
+
+console.log(animalIcons,vegetableIcons,userIcons);
